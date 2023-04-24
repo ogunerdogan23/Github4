c5dd5e4325d4e3c28f922785b33fe87ce9043664
Ogün Erdogan <ogun.erdogan23@gmail.com>
2023-04-24 19:26:10 +0200

    first commit

---
+++ src/G02.java
@@ -0,0 +1,28 @@
+public class G02 {
+    public static void main(String[] args) {
+        /*
+
+        Git-Github'da projelerimizin üzrinde calisirken temelde 3 adimda islem yapiyoruz
+
+        1- Projelerimizi/dosyalarimizi git ile iliskilendiriyoruz
+               git init
+
+        2- Projelerimizde/Dosyalarimizda bulunalari versiyon olusturuyoruz
+
+              git add .
+              git commit -m"versiyonadi"
+
+        3- github'a yollama ya da entegrasyon
+
+              git remote add origin repoURL adresi
+              (already origin derse)
+              git remote set-url repoURL adresi
+
+              git push
+
+
+
+
+         */
+    }
+}
